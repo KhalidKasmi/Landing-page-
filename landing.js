339a09bbd08525446d420230e552b055d57d2761
KhalidKasmi <kasmikhalid190@gmail.com>
2022-09-28 04:09:14 +0100

    adding main files

---
+++ landing.js
@@ -0,0 +1,12 @@
+let img = document.querySelector('.imagepric')
+let container = document.querySelector('.container')
+
+function phone(ph){
+    img.src = ph
+}
+
+function colorBack (color){
+    container.style.background = color
+
+}
+
